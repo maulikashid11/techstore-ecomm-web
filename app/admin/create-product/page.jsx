82db48444b545231adbe8de819443cf006a5fbaa
Mauli Kashid <maulikashid78@gmail.com>
2025-12-07 16:36:52 +0530

    fourth

--- app/admin/create-product/page.jsx
+++ app/admin/create-product/page.jsx
@@ -4,6 +4,7 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
 
 
 const CreateProduct = () => {
